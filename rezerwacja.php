42447a6fc2c1eb88d0f4eed4deaa1ea2eda74517
RobiPlay <156914260+RobiPlay@users.noreply.github.com>
2024-12-19 14:48:05 +0100

    Add files via upload
    Baba Ji

--- rezerwacja.php
+++ rezerwacja.php
@@ -1,5 +1,16 @@
 <?php
 if (isset($_POST["data"]) && isset($_POST["osob"]) && isset($_POST["numer"])){
+
+    $dara = $_POST['data'];
+    $ludzie = $_POST['ludzie'];
+    $tel = $_POST['tel'];
+    $server = 'localhost';
+    $user = 'root';
+    $password = "";
+    $database = 'baza';
+    $connect = mysqli_connect($server,$user,$password,$database);
+    $zapytanie = "INSERT INTO rezerwacje VALUES (DEFAULT,1, ".$_POST["data"].",".$_POST["osob"].",".$_POST["numer"].");"
+
     echo "Dodano rezerwację do bazy";
     $mysqli = new mysqli("localhost", "root", , "baza");
 
